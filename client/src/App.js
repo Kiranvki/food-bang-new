cbd7d18dd2266234db92aa25b9296d4b66cfe818
Kiranvki <kiran.1js15me407@gmail.com>
2022-09-15 18:08:45 +0530

    Updated loading spinner

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import About from './components/About';
 import Home from './components/Home';
@@ -7,7 +7,7 @@ import Menu from './Utili/Menu';
 import { ThemeProvider } from '@mui/material/styles';
 import Pnf from './Utili/Pnf';
 import LogIn from './components/Login';
-import { Separator, theme } from './styles/theme';
+import { Colors, Separator, theme } from './styles/theme';
 import Footer from './components/Footer';
 import SignUp from './components/SignUp';
 import SearchBox from './components/SearchBox';
@@ -33,13 +33,22 @@ import OrderList from './components/screens/admin/OrderList';
 import UpdateProduct from './components/screens/admin/UpdateProduct';
 import UpdateProfile from './components/screens/admin/UpdateProfile';
 import ResetPass from './components/screens/user/ResetPass';
-
+import BeatLoader from "react-spinners/PulseLoader";
+import PulseLoader from 'react-spinners/PulseLoader';
+import ScrollToTop from './hooks/Scrool';
 
 function App(props) {
 
+
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     document.title = "Food-Bang-Home"
-  })
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000
+    )
+  }, [])
 
   const context = useContext(GlobalState);
 
@@ -48,98 +57,114 @@ function App(props) {
   const [isUser, setIsUser] = context.authApi.isUser;
 
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <MenuBar />
-        <ToastContainer autoClose={2000} position="top-center" />
-        <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path={"/food-bang"} element={<Home />} />
-          <Route path={"/home"} element={<Home />} />
-          <Route path={"/about"} element={<About />} />
-          <Route path={"/vendor"} element={<Vendor />} />
-          <Route path={"/menulist"} element={<MenuList />} />
-          <Route path={"/login"} element={<LogIn />} />
-          <Route path={"/signup"} element={<SignUp />} />
-          <Route path={"/resetPass"} element={<ResetPass />} />
-          <Route path={'/add'} element={
-            <ProtectedRoute auth={isLogged}>
-              <Profile />
-            </ProtectedRoute>
-          } />
-          {/* <Route path={'/user/dashboard'} element={
-            <ProtectedRoute auth={isLogged}>
-              <UserDash />
-            </ProtectedRoute>
-          } /> */}
-          <Route path={'/users'} element={
-            <ProtectedRoute auth={isLogged}>
-              <Users />
-            </ProtectedRoute>
-          } />
-          <Route path={'/orders'} element={
-            <ProtectedRoute auth={isLogged}>
-              <Orders />
-            </ProtectedRoute>
-          } />
-          <Route path={'/admin/allOrders'} element={
-            <ProtectedRoute auth={isLogged}>
-              <OrderList />
-            </ProtectedRoute>
-          } />
+    <div>
+      {loading ? (
+        <PulseLoader
+          size={20}
+          color={Colors.secondary}
+          loading={loading}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        />
+      ) : (
+        <ThemeProvider theme={theme}>
+          <BrowserRouter>
+            <MenuBar />
+            <ToastContainer autoClose={2000} position="top-center" />
+            <ScrollToTop>
+              <Routes>
+                <Route path={"/"} element={<Home />} />
+                <Route path={"/food-bang"} element={<Home />} />
+                <Route path={"/home"} element={<Home />} />
+                <Route path={"/about"} element={<About />} />
+                <Route path={"/vendor"} element={<Vendor />} />
+                <Route path={"/menulist"} element={<MenuList />} />
+                <Route path={"/login"} element={<LogIn />} />
+                <Route path={"/signup"} element={<SignUp />} />
+                <Route path={"/resetPass"} element={<ResetPass />} />
+                <Route path={'/add'} element={
+                  <ProtectedRoute auth={isLogged}>
+                    <Profile />
+                  </ProtectedRoute>
+                } />
+                
+                <Route path={'/users'} element={
+                  <ProtectedRoute auth={isLogged}>
+                    <Users />
+                  </ProtectedRoute>
+                } />
+                <Route path={'/orders'} element={
+                  <ProtectedRoute auth={isLogged}>
+                    <Orders />
+                  </ProtectedRoute>
+                } />
+                <Route path={'/admin/allOrders'} element={
+                  <ProtectedRoute auth={isLogged}>
+                    <OrderList />
+                  </ProtectedRoute>
+                } />
 
-          <Route path={'/product/create'} element={
-            <ProtectedRoute auth={isLogged}>
-              <CreateProduct />
-            </ProtectedRoute>
-          } />
-          <Route path={'/product/details/:id'} element={
-            <ProtectedRoute auth={isLogged}>
-              <ProductDetail />
-            </ProtectedRoute>
-          } />
-          <Route path={'/profile'} element={
-            <ProtectedRoute auth={isLogged}>
-              <Profile />
-            </ProtectedRoute>
-          } />
-          <Route path={'/products'} element={
-            <ProtectedRoute auth={isLogged}>
-              <AdminProducts />
-            </ProtectedRoute>
-          } />
-          <Route path={'/checkout/:final'} element={
-            <ProtectedRoute auth={isLogged}>
-              <CheckOut />
-            </ProtectedRoute>
-          } />
-          <Route path={'/product/update/:id'} element={
-            <ProtectedRoute auth={isLogged}>
-              <UpdateProduct />
-            </ProtectedRoute>
-          } />
-          <Route path={'/profile/update/:id'} element={
-            <ProtectedRoute auth={isLogged}>
-              <UpdateProfile />
-            </ProtectedRoute>
-          } />
-          <Route path={'/product/cart'} element={
-            <ProtectedRoute auth={isLogged}>
-              <Cart />
-            </ProtectedRoute>
-          } />
-          <Route path={'/admin/dashboard'} element={
-            <ProtectedRoute auth={isLogged}>
-              <AdminDash />
-            </ProtectedRoute>
-          } />
-          <Route path={"/*"} element={<Pnf />} />
-        </Routes>
-        <SearchBox />
-        <Separator />
-        <Footer />
-      </BrowserRouter>
-    </ThemeProvider >
+                <Route path={'/product/create'} element={
+                  <ProtectedRoute auth={isLogged}>
+                    <CreateProduct />
+                  </ProtectedRoute>
+                } />
+                <Route path={'/product/details/:id'} element={
+                  <ProtectedRoute auth={isLogged}>
+                    <ProductDetail />
+                  </ProtectedRoute>
+                } />
+                <Route path={'/profile'} element={
+                  <ProtectedRoute auth={isLogged}>
+                    <Profile />
+                  </ProtectedRoute>
+                } />
+                <Route path={'/products'} element={
+                  <ProtectedRoute auth={isLogged}>
+                    <AdminProducts />
+                  </ProtectedRoute>
+                } />
+                <Route path={'/checkout/:final'} element={
+                  <ProtectedRoute auth={isLogged}>
+                    <CheckOut />
+                  </ProtectedRoute>
+                } />
+                <Route path={'/product/update/:id'} element={
+                  <ProtectedRoute auth={isLogged}>
+                    <UpdateProduct />
+                  </ProtectedRoute>
+                } />
+                <Route path={'/profile/update/:id'} element={
+                  <ProtectedRoute auth={isLogged}>
+                    <UpdateProfile />
+                  </ProtectedRoute>
+                } />
+                <Route path={'/product/cart'} element={
+                  <ProtectedRoute auth={isLogged}>
+                    <Cart />
+                  </ProtectedRoute>
+                } />
+                <Route path={'/admin/dashboard'} element={
+                  <ProtectedRoute auth={isLogged}>
+                    <AdminDash />
+                  </ProtectedRoute>
+                } />
+                <Route path={"/*"} element={<Pnf />} />
+
+              </Routes>
+            </ScrollToTop>
+            <SearchBox />
+            <Separator />
+            <Footer />
+          </BrowserRouter>
+        </ThemeProvider >
+      )}
+    </div>
+
   );
 }
 
